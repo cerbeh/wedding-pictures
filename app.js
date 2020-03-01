6654df2dcc622a3df59c21355458befc5a8a33b3
Cerbeh <cerbeh@gmail.com>
2020-03-01 19:25:13 +0000

    Added dependencies and configured pictures endpoint

---
+++ app.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = process.env.port || 3030;
+
+const cloudinary = require('cloudinary').v2;
+
+app.use(bodyParser.json());
+
+app.get('/pictures', (req, res) => {
+  cloudinary.search
+    .expression('folder:Wedding')
+    .sort_by('public_id','desc')
+    .max_results(30)
+    .execute()
+    .then(result => res.json(result.resources))
+    .catch(e => console.error('Error getting resource from cloudinary ::', e));
+});
+
+app.listen(PORT, () => console.log(`Server up and running on port: ${PORT}`));
