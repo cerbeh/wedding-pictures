70c2965e9d63529d3afb1a70bbf0737dd4074690
Cerbeh <cerbeh@gmail.com>
2020-03-01 19:43:12 +0000

    Added next_cursor as part of cloudinary request to allow pagination

--- app.js
+++ app.js
@@ -12,9 +12,11 @@ app.get('/pictures', (req, res) => {
   cloudinary.search
     .expression('folder:Wedding')
     .sort_by('public_id','desc')
+    .next_cursor(req.body.next_cursor)
     .max_results(30)
     .execute()
-    .then(result => res.json(result.resources))
+
+    .then(result => res.json(result))
     .catch(e => console.error('Error getting resource from cloudinary ::', e));
 });
 
